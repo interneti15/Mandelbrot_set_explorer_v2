b2c42831feb5af1694c9b3687d78e703ca682e0a
interneti15 <interneti15tv@gmail.com>
2023-12-05 18:58:49 +0100

    Update Mandelbrot_set_explorer_v2.cpp
    sync

--- Mandelbrot_set_explorer_v2/Mandelbrot_set_explorer_v2.cpp
+++ Mandelbrot_set_explorer_v2/Mandelbrot_set_explorer_v2.cpp
@@ -18,7 +18,14 @@ public:
 	int operator=(long double other)
 	{
 		int cother = (int)other;
+		int temp_num;
 		size_t t_size = 0;
+
+		if (other < 0)
+		{
+			sign = false;
+		}
+
 		if (cother > 1)
 		{
 			for (size_t i = 1;; i++)
@@ -30,22 +37,29 @@ public:
 				}
 			}
 
-			for (size_t i = t_size; i >= 0; i--)
+			for (int i = t_size; i >= 0; i -= 1)
 			{
-				data_pre_point.push_back((cother %(((int)pow(10, i)))));
+				temp_num = cother / (((int)pow(10, i)));
+				data_pre_point.push_back((temp_num));
+				cother -= temp_num * pow(10, i);
+				other -= temp_num * pow(10, i);
 			}
+		}
+
+		for (size_t i = 0;; i--)
+		{
 
 		}
 		
-		//return pow(10, t_size);
-		
 	}
+
+	vector<int> f1() { return data_pre_point; }
 
 protected:
 
 	bool sign = true;
-	vector<char> data_pre_point;
-	vector<char> data_afer_point;
+	vector<int> data_pre_point;
+	vector<int> data_afer_point;
 
 };
 
@@ -53,7 +67,14 @@ protected:
 int main()
 {
 	BigFloat a;
+	a = 123;
 
+	vector<int> ab = a.f1();
+
+	for (size_t i = 0; i < ab.size(); i++)
+	{
+		cout << ab[i];
+	}
 	
 }
 
