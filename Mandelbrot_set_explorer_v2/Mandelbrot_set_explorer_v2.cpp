e12036ec1c77c512b8e5cf65a44a03a66da9a0f2
Szymon Lipski <interneti15tv@gmail.com>
2023-12-06 01:39:23 +0100

    starting cords

--- Mandelbrot_set_explorer_v2/Mandelbrot_set_explorer_v2.cpp
+++ Mandelbrot_set_explorer_v2/Mandelbrot_set_explorer_v2.cpp
@@ -12,25 +12,70 @@
 using namespace boost::multiprecision;
 using namespace std;
 
-constexpr int WIDTH = 900;
-constexpr int HEIGHT = 900;
+namespace global
+{
+	constexpr int WIDTH = 900;
+	constexpr int HEIGHT = 900;
 
-std::vector<int> screen(WIDTH* HEIGHT, 0);
-sf::Uint8* pixels = new sf::Uint8[WIDTH * HEIGHT * 4];
+	vector<int> screen(WIDTH* HEIGHT, 0);
+	sf::Uint8* pixels = new sf::Uint8[WIDTH * HEIGHT * 4];
 
-std::mutex screenMutex;
+	mutex screenMutex;
+}
 
-class Positions
+class point // just a pair of numbers
 {
 public:
-	cpp_dec_float_50 x_middle = 0;
-	cpp_dec_float_50 y_middle = 0;
+	cpp_dec_float_50 x = 0;
+	cpp_dec_float_50 y = 0;
 
-	cpp_dec_float_50 Top_Left = 0;
-	cpp_dec_float_50 Top_right = 0;
+	point set_point(const cpp_dec_float_50& x, const cpp_dec_float_50& y)
+	{
+		this->x = x;
+		this->y = y;
 
-	cpp_dec_float_50 Down_Left = 0;
-	cpp_dec_float_50 Down_Right = 0;
+		return *this;
+	}
+};
+
+class positions // positions and handling coordinates and edge points
+{
+public:
+
+	point top_left;
+	point top_right;
+	point down_left;
+	point down_right;
+
+	cpp_dec_float_50 step;
+
+	positions() // starting positions dependent on Width and Height
+	{
+		if (global::WIDTH >= global::HEIGHT)
+		{
+			step = 4 / (global::HEIGHT - 1);
+
+			top_left.y = 2;
+			top_left.x = step * ((global::WIDTH - 1) / 2);
+		}
+		else
+		{
+			step = 4 / (global::WIDTH - 1);
+
+			top_left.x = -2;
+			top_left.y = step * ((global::HEIGHT - 1) / 2);
+		}
+
+		down_left.x = top_left.x;
+		down_left.y = top_left.y - ((global::HEIGHT - 1) * step);
+
+		top_right.y = top_left.y;
+		top_right.x = top_left.x + ((global::WIDTH - 1) * step);
+
+		down_right.x = top_right.x;
+		down_right.y = down_left.y;
+		
+	}
 
 	static void recalculate()
 	{
